93fd1db9c986465fd035c50f7c8657e8c95d0e1e
goutam96801 <gautamkumar96801@gmail.com>
2023-06-25 10:05:24 +0530

    Completed textutils

---
+++ src/components/TextForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+
+export default function TextForm(props) {
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to Uppercase", "success");
+
+    }
+
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to Lowercase", "success");
+
+    }
+
+    const handleClrClick = () => {
+        let newText = ''
+        setText(newText)
+        props.showAlert("Text Cleared", "success");
+    }
+
+    const handleCopyClick = () => {
+        var text = document.getElementById("myBox");
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to Clipboard", "success");
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+
+    }
+    const [text, setText] = useState("");
+    return (
+        <>
+            <div className='container' style={{ color: props.mode === 'dark' ? '#fff' : '#343a40' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : '#fff', color: props.mode === 'dark' ? '#fff' : '#343a40' }} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary m-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary m-1" onClick={handleLowClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary m-1" onClick={handleClrClick}>Clear Text</button>
+                <button className="btn btn-primary m-1" onClick={handleCopyClick}>Copy Text</button>
+            </div >
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? '#fff' : '#343a40' }}>
+                <h2>Your Text Summary ↴</h2>
+                <p>{text.split(" ").length - 1} words and {text.length} characters</p>
+                <p>{0.008 * text.length} Minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter Something  in the textbox above to previw it"}</p>
+            </div>
+
+        </>
+    )
+}
